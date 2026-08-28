b6d246f5473f279e8ed6c6100c791cafcabf8cd0
Micah <micahra@users.noreply.github.com>
2026-08-28 14:08:08 -0500

    Create Repair-DomainTrust.ps1

---
+++ Powershell/Active Directory/Repair-DomainTrust.ps1
@@ -0,0 +1,5 @@
+# This program will reconnect an orphaned computer back to the domain.
+# Fixes "trust relationship between this workstation and the primary domain failed"
+
+$credential = Get-Credential
+Test-ComputerSecureChannel -Repair -Credential $credential
