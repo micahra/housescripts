c4a8e678e993d570e57ab62f518df18e2df4756a
Micah <micahra@users.noreply.github.com>
2026-09-14 15:45:43 -0500

    added file-size checking and new file creator

--- Powershell/General Admin/Ping-RemoteAddress.ps1
+++ Powershell/General Admin/Ping-RemoteAddress.ps1
@@ -4,10 +4,24 @@
 
 $REMOTE_ADDRESS = Read-Host("Enter Remote IP: ")
 $BASE_FILE = "remote_ping.txt"
-$FILE_NAME_COUNTER = 0
+$CURRENT_FILE = $BASE_FILE
+$FILE_NAME_COUNTER = 0 
+$MAX_FILE_SIZE_BYTES = 2KB
 
 
 ping.exe -t $REMOTE_ADDRESS | ForEach-Object {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_"} | Tee-Object -FilePath "remote_ping.txt"
 
+while ($true) {
+    ping.exe -t $REMOTE_ADDRESS | ForEach-Object {
+        if (Test-Path $CURRENT_FILE) {
+            $file_size = (Get-Item $CURRENT_FILE).Length
+            if ($file_size -ge $MAX_FILE_SIZE_BYTES) {
+                $FILE_NAME_COUNTER++
+                $CURRENT_FILE = "remote_ping$FILE_NAME_COUNTER.txt"
+                Write-Host "`n[Log Rotated] Size limit reached. New log file created: $CURRENT_FILE"
+            }
+        }
+    }_ | Tee-Object -FilePath $CURRENT_FILE -Append
+}
 # make Tee-Object create a new file when file reaches certain size
 # append file numering to end of file name when files reach a certain size
