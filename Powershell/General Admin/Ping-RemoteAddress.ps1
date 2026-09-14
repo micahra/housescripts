d254fb8de95827597ac4019d94908e0e027e1905
Micah <micahra@users.noreply.github.com>
2026-09-14 15:57:35 -0500

    Fixed Log size rotation and date output

--- Powershell/General Admin/Ping-RemoteAddress.ps1
+++ Powershell/General Admin/Ping-RemoteAddress.ps1
@@ -9,10 +9,10 @@ $FILE_NAME_COUNTER = 0
 $MAX_FILE_SIZE_BYTES = 2KB
 
 
-ping.exe -t $REMOTE_ADDRESS | ForEach-Object {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_"} | Tee-Object -FilePath "remote_ping.txt"
-
+# ping.exe -t $REMOTE_ADDRESS | ForEach-Object {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_"} | Tee-Object -FilePath "remote_ping.txt"
 while ($true) {
     ping.exe -t $REMOTE_ADDRESS | ForEach-Object {
+        
         if (Test-Path $CURRENT_FILE) {
             $file_size = (Get-Item $CURRENT_FILE).Length
             if ($file_size -ge $MAX_FILE_SIZE_BYTES) {
@@ -21,7 +21,10 @@ while ($true) {
                 Write-Host "`n[Log Rotated] Size limit reached. New log file created: $CURRENT_FILE"
             }
         }
-    }_ | Tee-Object -FilePath $CURRENT_FILE -Append
+    
+    "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_" | Tee-Object -FilePath $CURRENT_FILE -Append
+    }
 }
+
 # make Tee-Object create a new file when file reaches certain size
 # append file numering to end of file name when files reach a certain size
