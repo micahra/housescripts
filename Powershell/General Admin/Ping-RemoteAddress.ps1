61f7e34d618dead53c857063bd395e30582e70ac
Micah <micahra@users.noreply.github.com>
2026-09-14 15:35:19 -0500

    Update Ping-RemoteAddress.ps1
    updated variable names

--- Powershell/General Admin/Ping-RemoteAddress.ps1
+++ Powershell/General Admin/Ping-RemoteAddress.ps1
@@ -2,10 +2,12 @@
 # This script will ping a remote address with timestamps, and also echo to a text file, located at %userprofile%\Documents\remote_ping.txt
 
 
-$remote_address = Read-Host("Enter Remote IP: ")
+$REMOTE_ADDRESS = Read-Host("Enter Remote IP: ")
+$BASE_FILE = "remote_ping.txt"
+$FILE_NAME_COUNTER = 0
 
 
-ping.exe -t $remote_address | ForEach-Object {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_"} | Tee-Object -FilePath "remote_ping.txt"
+ping.exe -t $REMOTE_ADDRESS | ForEach-Object {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_"} | Tee-Object -FilePath "remote_ping.txt"
 
 # make Tee-Object create a new file when file reaches certain size
 # append file numering to end of file name when files reach a certain size
