37d20b299ce8e4d6adea76a9a919d1c6e01bbe67
Micah <micahra@users.noreply.github.com>
2026-09-14 15:23:16 -0500

    edited remote_ping.txt output location

--- Powershell/General Admin/Ping-RemoteAddress.ps1
+++ Powershell/General Admin/Ping-RemoteAddress.ps1
@@ -1,6 +1,8 @@
-# This script will ping a remote address with timestamps, and also echo to a text file, located at C:\remote_ping.txt
+# This is a windows script
+# This script will ping a remote address with timestamps, and also echo to a text file, located at %userprofile%\Documents\remote_ping.txt
+
 
 $remote_address = Read-Host("Enter Remote IP: ")
 
 
-ping.exe -t $remote_address | ForEach-Object {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_"} | Tee-Object -FilePath "C:\remote_ping.txt"
+ping.exe -t $remote_address | ForEach-Object {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $_"} | Tee-Object -FilePath "remote_ping.txt"
